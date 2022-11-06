e0c54c7d1ab0965eafd108ec74863cc52e72d2f9
NLarinov <tkachevkolya2006@gmail.com>
2022-11-06 15:31:38 +0300

    Commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('Hello world')
+from PyQt5.QtWidgets import QMainWindow, QPushButton
+
+
+class Aloitus(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.button = QPushButton("Ok", self)
+        self.button.move(200, 200)
+        self.button.clicked.connect(self.continue2)
+        self.setGeometry(600, 200, 500, 300)
+        self.show()
+
+    def continue2(self):
+        self.close()
+        _ = Second()
+
+
+class Second(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = "Alkutiedot"
+        self.top = 600
+        self.left = 200
+        self.width = 500
+        self.height = 500
+
+        self.button = QPushButton("Ok", self)
+        self.button.move(100, 400)
+        self.button.clicked.connect(self.ok)
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
+
+    def ok(self):
+        print('close clicked')
+        self.close()
