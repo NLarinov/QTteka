b41c04599079b6028c522660ec4e9403a4fb3fe2
NLarinov <tkachevkolya2006@gmail.com>
2022-11-12 23:58:50 +0300

    SECOND BIG COMMIT

--- jett.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-con = sqlite3.connect('static/Films.db')
-cur = con.cursor()
-count = cur.execute("""SELECT position, name, date FROM Watchlater ORDER BY position""").fetchall()
-con.commit()
-con.rollback()
-con.close()
-print(count)
